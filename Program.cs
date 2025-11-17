d345f8d1c8d2ccc9b5fdd3492015fbad3c98bbbb
Robert Gleason <l2ob122@gmail.com>
2025-11-16 21:02:49 -0800

    feat: reorganize project structure and enhance layout components

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
+using BlazorIntro.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorIntro;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
